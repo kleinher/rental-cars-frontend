fecc280694e1cea05a26fd3c59cea8527dd6f6c3
Hernan Kleinubing <hernan.k97@gmail.com>
2025-01-08 21:25:49 +0100

    data act

--- src/user/UserProfile.js
+++ src/user/UserProfile.js
@@ -1,34 +1,25 @@
 import React, { useState } from "react";
 import { cars } from "../Data";
-import { Box, Card, CardContent, Typography, TextField, Button } from "@mui/material";
+import { Box, Card, CardContent, Typography, TextField, Button, Grid2 } from "@mui/material";
+
 
 const UserProfile = ({ userId }) => {
-    const [kilometers, setKilometers] = useState("");
-    const userCar = cars.find((car) => car.id === userId);
-
-    const handleUpdate = () => {
-        alert(`Kilómetros actualizados a: ${kilometers}`);
-        setKilometers(""); // Limpia el input después de actualizar
-    };
+    const userCars = cars.filter((car) => car.userId === userId);
 
     return (
-        <Box sx={{ p: 4 }}>
+        <Box sx={{ p: 4, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <Card>
-                <CardContent>
-                    <Typography variant="h5" gutterBottom>Perfil del Usuario</Typography>
-                    <Typography variant="body1">Coche asignado: {userCar.model}</Typography>
-                    <Typography variant="body1">Kilómetros actuales: {userCar.kilometers}</Typography>
-                    <TextField
-                        label="Actualizar kilómetros"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        value={kilometers}
-                        onChange={(e) => setKilometers(e.target.value)}
-                    />
-                    <Button variant="contained" color="primary" onClick={handleUpdate}>
-                        Actualizar Kilómetros
-                    </Button>
+                <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                    <Typography variant="h5" gutterBottom>Pedro Enrique Conqui</Typography>
+                    <Grid2 container spacing={2}>
+                        {userCars.map((car) => (
+                            <Grid2 item xs={12} key={car.id}>
+                                <Button variant="outlined" fullWidth sx={{ height: '100px' }}>
+                                    {car.licensePlate}
+                                </Button>
+                            </Grid2>
+                        ))}
+                    </Grid2>
                 </CardContent>
             </Card>
         </Box>
@@ -36,4 +27,3 @@ const UserProfile = ({ userId }) => {
 };
 
 export default UserProfile;
-
